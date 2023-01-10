b42af954dee775607c064fc72b44e881bf15f005
Sebastian Korzeniecki <seba5zer@gmail.com>
2023-01-10 21:00:37 +0100

    Add some methods

---
+++ src/Util/HttpHeader.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace S3bul\CurlPsr7\Util;
+
+abstract class HttpHeader
+{
+    const AUTHORIZATION = 'Authorization';
+    const CONTENT_TYPE = 'Content-Type';
+
+}
